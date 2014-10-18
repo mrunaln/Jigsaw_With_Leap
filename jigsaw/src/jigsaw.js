b5c63385cd7d9bcfd647c230b5233ad6d7e497c4
Mrunal Nargunde <mrunalnargunde@gmail.com>
2014-10-18 19:23:58 -0400

    Canvas overlaps the jigsaw boards.
    Pointers move with increased sensitivity.

--- jigsaw/src/jigsaw.js
+++ jigsaw/src/jigsaw.js
@@ -1,38 +1,33 @@
 var zindex = 0;
 
-var controllerOptions = { enableGestures: true },
-   canvas = d3.select('div#wrapper_container') 
-                .append('canvas')
-                .attr({
-                        "width": screen.width,
-                        "height": screen.height,
-                        "z-index" : zindex
-                    }).node(),
+// fullscreen
+var canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 // create a rendering context
- ctx = canvas.getContext("2d");
- ctx.translate(canvas.width,canvas.height);
- ctx.fillStyle = "rgba(0,0,0,0.7)";
-
+var ctx = canvas.getContext("2d");
+ctx.translate(canvas.width/2,canvas.height);
+ctx.fillStyle = "rgba(0,0,0,0.7)";
 
 // render each frame
 function draw(obj) {
-    
   // clear last frame
-  ctx.clearRect(-canvas.width,-canvas.height,canvas.width,canvas.height);
+  ctx.clearRect(-canvas.width/2,-canvas.height,canvas.width,canvas.height);
 
   // render circles based on pointable positions
   var pointablesMap = obj.pointablesMap;
   for (var i in pointablesMap) {
-     // get the pointable's position
-      var pointable = pointablesMap[i];
-       var pos = pointable.tipPosition;
-      // create a circle for each pointable
-       var radius = Math.min(600/Math.abs(pos[2]),20);
-       ctx.beginPath();
-       ctx.arc(pos[0]-radius/2,-pos[1]-radius/2,radius,0,2*Math.PI);
-       ctx.fill();
-   }
+    // get the pointable's position
+    var pointable = pointablesMap[i];
+    var pos = pointable.tipPosition;
+
+    // create a circle for each pointable
+    var radius = Math.min(600/Math.abs(pos[2]),20);
+    ctx.beginPath();
+    ctx.arc((pos[0]-radius/2)*3,(-pos[1]-radius/2)*3,radius,0,2*Math.PI);
+    ctx.fill();
+  }
 };
 
 $(document).ready(function(){
@@ -59,7 +54,10 @@ $(document).ready(function(){
 });
 
 function hideSolution(id){
+  $('#'+id).parent().removeClass("individualPieces");
+  /*FIXME - i dont know why border class not seen after jumble button pressed.*/
    $('#'+id).parent().addClass("hiddenIndividualPieces");
+   
 }
 
 function drawImage(imageSrc, canvasID){
