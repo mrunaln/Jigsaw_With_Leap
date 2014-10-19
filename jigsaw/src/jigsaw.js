2b26d9a598fbba2fd3462753e5a190e465def00e
Mrunal Nargunde <mrunalnargunde@gmail.com>
2014-10-19 01:27:48 -0400

    Dynamic use of controller, gesture detection basics, hand usage
    Comments inline will be helpful.
    Connect /disconnect controller handled gracefully.
    Basic snippet showing hands obj usage
    Crude solution for 2 points only

--- jigsaw/src/jigsaw.js
+++ jigsaw/src/jigsaw.js
@@ -1,29 +1,41 @@
-var zindex = 0;
 
 // fullscreen
 var canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
+width = window.innerWidth;
+height = window.innerHeight;
 // create a rendering context
 var ctx = canvas.getContext("2d");
 ctx.translate(canvas.width/2,canvas.height);
 ctx.fillStyle = "rgba(0,0,0,0.7)";
 
 // render each frame
-function draw(obj) {
+function draw(obj) 
+{
   // clear last frame
   ctx.clearRect(-canvas.width/2,-canvas.height,canvas.width,canvas.height);
 
   // render circles based on pointable positions
   var pointablesMap = obj.pointablesMap;
-  for (var i in pointablesMap) {
-    // get the pointable's position
+   var masterFinger=-1, slaveFinger=-2;
+   counter = 0;
+  for (var i in pointablesMap) 
+  {
+    /* 
+    Currently Working : Draw only 2 pointers
+    FIXME  - Find a cleaner solution 
+    */
+    if(counter > 1 ){
+      counter = 0 ;
+      console.log("Draw only 2 pointers");
+      break;
+    } 
+    counter += 1;
     var pointable = pointablesMap[i];
     var pos = pointable.tipPosition;
-
     // create a circle for each pointable
-    var radius = Math.min(600/Math.abs(pos[2]),20);
+    var radius = 20;
     ctx.beginPath();
     ctx.arc((pos[0]-radius/2)*3,(-pos[1]-radius/2)*3,radius,0,2*Math.PI);
     ctx.fill();
@@ -70,4 +82,87 @@ function drawImage(imageSrc, canvasID){
     context.drawImage(img, 0, 0,canvas.width, canvas.height);
 }
 
-Leap.loop(draw);
+
+// Creates our Leap Controller
+    var controller = new Leap.Controller({enableGestures:true});
+
+    // Tells the controller what to do every time it sees a frame
+    controller.on( 'frame' , function( data ){
+
+      // Assigning the data to the global frame object
+      frame = data;
+      // Clearing the drawing from the previous frame
+      ctx.clearRect( 0 , 0 , width , height );
+
+      /*
+      Snippet below works with hands object.
+      Tried to execute and read regarding rotate hand.
+      Unable find a clear rotation angle yet.
+      Not sure if will use hands approach.
+      Reference : 
+      https://developer.leapmotion.com/documentation/javascript/api/Leap.Hand.html#fingers[]
+      */
+      if(frame.hands.length > 0)
+      {
+        var hand = frame.hands[0];
+        console.log(" hand - " + frame.hands[0]);
+        var previousFrame = controller.frame(1);
+        var totalRotation = hand.rotationAngle(previousFrame);
+        var rotationAroundZAxis = hand.rotationAngle(previousFrame, [0,0,1]);
+        console.log("Rot: " + totalRotation + ", Z Rot:" + rotationAroundZAxis);
+      }
+      draw(frame);
+      
+
+      /*
+      Snippet below shows how to work with gestures.
+      Not sure if I will be using these.
+      Reference : 
+      http://goo.gl/Zl8XBH
+      or
+      http://goo.gl/V8eIjQ
+      */
+      for( var i = 0; i < frame.gestures.length; i++ ){
+        var gesture = frame.gestures[i];
+        var type = gesture.type;
+        switch( type )
+        {  
+          case "circle":
+            //onCircle( gesture );
+            console.log ("CIRCLE");
+            break;
+            
+          case "swipe":
+            console.log ("SWIPE");
+            break;
+          
+          case "screenTap":
+            console.log ("SCREEN TAP");
+            break;
+
+          case "keyTap":
+            console.log ("KEY TAP");
+            break;
+        }     
+      }
+    });
+
+/*
+Handling if the device gets disconnected in between.
+Also can withdraw fingers and start again seamlessly. 
+No need to reload the page as before.
+*/    
+controller.on('connect', function() {
+  console.log("Successfully connected.");
+});
+
+controller.on('deviceConnected', function() {
+  console.log("A Leap device has been connected.");
+});
+
+controller.on('deviceDisconnected', function() {
+  console.log("A Leap device has been disconnected.");
+});
+
+controller.connect();
+
